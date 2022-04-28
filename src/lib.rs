af8a4fe58e732fef43b6f00d192fb89bdcf7588f
song <suagrfillet@gmail.com>
2022-04-28 14:04:42 +0800

    chars 0.0.2
    1. add wheel counter support
    2. simple paras parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,62 @@
 use std::collections::HashMap;
-use evdev::{Device, EventType, InputEvent};
-use evdev::InputEventKind::Key;
+use evdev::{Device, EventType, InputEvent,  RelativeAxisType};
+use evdev::InputEventKind::{Key, RelAxis};
+
 
 pub struct KeyHit {
     name : String ,
     device : String,
     types : EventType,
-    count : HashMap<evdev::Key,u64> ,
+    count : HashMap<u16,u64> ,
 }
 
 impl KeyHit {
 
-    pub fn new(name:String,device:String,types:EventType,count:HashMap<evdev::Key,u64>) -> KeyHit {
+    pub fn new(name:String,device:String,types:EventType,count:HashMap<u16,u64>) -> KeyHit {
         KeyHit { name, device, types, count, }
     }
 
     pub fn do_count(&mut self, ev: InputEvent) {
-        if ev.event_type() == self.types && ev.value() == 0 {
-            if let Key(k) = ev.kind() {
-                let count = self.count.entry(k).or_insert(0);
-                *count += 1;
-                self.print_counter();
-            }
+        // YYYYYYYYYYYYY RelAxis(REL_WHEEL_HI_RES)
+        // https://patchwork.kernel.org/project/linux-input/patch/20181121152712.6770-8-benjamin.tissoires@redhat.com/
 
-        }
+         match ev.kind(){
+             // deal keyboard and mouse keys
+            Key(kk) if ev.value() == 0  => {
+                self.add_counter(kk.code())
+            },
+             // deal mouse wheel roll
+            RelAxis(ww) =>
+                if ww == RelativeAxisType::REL_WHEEL {
+                    self.add_counter(0xffff)
+            },
+            _ => {},
+        };
     }
     pub fn open_device(&self) -> Device {
         Device::open(&self.device).unwrap()
     }
 
-    fn print_counter(&self){
+    fn add_counter(&mut self, k : u16 ){
 
+        // increase
+        let count = self.count.entry(k).or_insert(0);
+        *count += 1;
+
+        // summary
+        let mut cc: u64 = 0 ;
         println!("XXXXXXXX {} XXXXXXXXXX",self.name);
+
         for (k,v) in &self.count{
-            println!("key is {:?} counter is {}",k,v);
-        }
+            if 0xffff  == *k {
+                println!("key is REL_WHEEL counter is {}",v);
+            } else {
+                println!("key is {:?} counter is {}", evdev::Key::new(*k), v);
+            }
+                cc += v;
+        };
+
+        println!("XXXXXXXX {} XXXXXXXXXX",cc);
         println!();
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,62 @@
 use std::collections::HashMap;
+use std::env::Args;
+use std::process::exit;
 use std::thread;
 use evdev::EventType;
 use chars::KeyHit;
 
+fn do_help(err : &str) -> ! {
+
+    eprintln!("Please input correctly Bro, Like this \n# sudo chars -k /dev/input/event5 -m /dev/input/event14");
+    if err.len() != 0 {
+
+        eprintln!("Detailed Error is {}",err);
+    }
+    exit(2);
+}
+
+fn parse_args(mut list :Args) -> Result<(String,String),&'static str>{
+    list.next();
+    list.next();
+
+    let x = match list.next() {
+        Some(x) => x,
+        None => return Err("get keyboard failed")
+    };
+
+    list.next();
+
+    let y = match list.next() {
+        Some(x) => x,
+        None => return Err("get mouse failed")
+    };
+
+    Ok((x,y))
+}
+
+
 fn main() {
 
- //   let mut args= std::env::args();
+    let  args= std::env::args();
+    if args.len() != 5 {
+        do_help("");
+    }
+    let (kd,md) = parse_args(args).unwrap_or_else(|err|{ do_help(err);});
 
     let k_counter = HashMap::new();
     let m_counter = HashMap::new();
 
-    let mut keyboard = KeyHit::new("keyboard".to_string(), "/dev/input/event4".to_string(), EventType::KEY, k_counter);
-    let mut mouse = KeyHit::new("mouse".to_string(), "/dev/input/event13".to_string(), EventType::KEY, m_counter);
+    let mut keyboard = KeyHit::new(
+        "keyboard".to_string(),
+        kd,
+        EventType::KEY,
+        k_counter);
 
-    //let mut listeners = vec![ keyboard,mouse, ];
-
-   // let key = &mut listeners[0];
-
+    let mut mouse = KeyHit::new(
+        "mouse".to_string(),
+        md,
+        EventType::KEY,
+        m_counter);
 
     let _handle1 = thread::spawn( move || {
         let key = &mut keyboard;
